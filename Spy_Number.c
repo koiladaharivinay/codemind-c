c05e588bbf9d9f4155a7b546c1b151e171725037
koiladaharivinay <100017894+koiladaharivinay@users.noreply.github.com>
2022-02-19 14:41:09 +0530

    #include<stdio.h> int main() { 	int n,i,s=0,p=1; 	scanf("%d",&n); 	for(i=1;n>0;i++) 	{ 	  i=n%10; 	  n=n/10;
    	  s=s+i;
    	  p=p*i;
    	}
    	if(p==s)
    	{
    		printf("Spy Number");
    	}
    	else
    	{
    		printf("Not Spy Number");
    	}
    }

---
+++ Spy_Number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,s=0,p=1;
+	scanf("%d",&n);
+	for(i=1;n>0;i++)
+	{
+	  i=n%10;
+	  n=n/10;
+	
+	  s=s+i;
+	  p=p*i;
+	}
+	if(p==s)
+	{
+		printf("Spy Number");
+	}
+	else
+	{
+		printf("Not Spy Number");
+	}
+}
+
