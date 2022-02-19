cfe8a81088a04125563ad944a8969713525ea95c
koiladaharivinay <100017894+koiladaharivinay@users.noreply.github.com>
2022-02-19 13:54:13 +0530

    #include<stdio.h> #include<math.h> int main() {     float r,area;     float pi=3.14;     scanf("%f",&r);     area=pi*pow(r,2);
        printf("%.2f",area);
        return 0;
    
        
    }

---
+++ Area_of_a_circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float r,area;
+    float pi=3.14;
+    scanf("%f",&r);
+    area=pi*pow(r,2);
+    
+    printf("%.2f",area);
+    return 0;
+
+    
+}
